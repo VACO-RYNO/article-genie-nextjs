c05f4582231ce2bc6c64314509cb6201d9361dfc
changyun-go <aeiou75three@gmail.com>
2022-06-13 16:53:42 +0900

    ✨ 새로운 구글 API 클라이언트 라이브러리로 migration

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          src="https://accounts.google.com/gsi/client"
+          async
+          defer
+        ></script>
+      </Head>
       <body>
         <div id="portal" />
         <Main />
