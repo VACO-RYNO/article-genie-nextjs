4b576c99a0273e50bbf7b8a67aeed50f49c0a7ae
junbro <khm11904@gmail.com>
2022-06-13 12:18:51 +0900

    ✨ SusPense, ErrorBoundary를 이용한 메인페이지 로딩 및 에러 처리

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRecoilValue } from "recoil";
@@ -10,6 +10,7 @@ import RecentlyViewedSites from "../components/RecentlyViewedSites";
 import AddressBar from "../components/AddressBar";
 
 import { isLoginState } from "../lib/recoil/auth";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function MainPage() {
   const isLogin = useRecoilValue(isLoginState);
@@ -26,10 +27,16 @@ export default function MainPage() {
         <meta name="description" content="article genie main page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Wrapper>
-        <AddressBar />
-        {!isSSR && isLogin ? <RecentlyViewedSites /> : <RecommendedSites />}
-      </Wrapper>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <ErrorBoundary
+          fallbackRender={({ error }) => <div>{error.message}</div>}
+        >
+          <Wrapper>
+            <AddressBar />
+            {!isSSR && isLogin ? <RecentlyViewedSites /> : <RecommendedSites />}
+          </Wrapper>
+        </ErrorBoundary>
+      </Suspense>
     </>
   );
 }
