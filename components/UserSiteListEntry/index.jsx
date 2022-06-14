848480938048455c706c06139a5a00d3c50c51b0
junbro <khm11904@gmail.com>
2022-06-14 18:24:21 +0900

    ✨ 최근 방문 사이트 목록 favicon.ico 추가

--- components/UserSiteListEntry/index.jsx
+++ components/UserSiteListEntry/index.jsx
@@ -2,9 +2,22 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 function UserSiteListEntry({ url }) {
+  const sourceDomain = url.slice(`https://`.length).split("/").shift();
+
   return (
     <Wrapper>
-      <p>{url}</p>
+      <Span>
+        <FaviconImg
+          src={`https://${sourceDomain}/favicon.ico`}
+          onError={e => (e.target.src = "/images/genie-logo.png")}
+        />
+      </Span>
+      <TitleWrapper>
+        <UrlTitle>{url}</UrlTitle>
+        <DomainName>
+          <span>{sourceDomain}</span>
+        </DomainName>
+      </TitleWrapper>
     </Wrapper>
   );
 }
@@ -13,17 +26,53 @@ UserSiteListEntry.propTypes = {
   url: PropTypes.string.isRequired,
 };
 
+const Span = styled.span`
+  border-radius: 4px;
+  float: left;
+  height: 18px;
+  line-height: 18px;
+  margin: 2px 20px 0 0;
+  overflow: hidden;
+  text-align: center;
+  width: 18px;
+`;
+
+const DomainName = styled.span`
+  color: #999;
+  float: right;
+  font-size: 14px;
+  line-height: 19px;
+  margin-left: 14px;
+`;
+
 const Wrapper = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  justify-content: center;
+  position: relative;
+  padding: 30px 10px;
 `;
 
 const FaviconImg = styled.img`
-  width: 16px;
-  height: 16px;
-  line-height: 16px;
+  position: absolute;
+  float: left;
+  width: 18px;
+  height: 18px;
+  line-height: 18px;
+`;
+
+const UrlTitle = styled.span`
+  color: #121212;
+  display: block;
+  font-size: 14px;
+  line-height: 19px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  word-break: break-all;
+`;
+
+const TitleWrapper = styled.div`
+  width: 500px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default UserSiteListEntry;
