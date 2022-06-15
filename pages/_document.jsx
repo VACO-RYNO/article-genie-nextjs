c5bfbcaf841cd7d9a74fdc7bd22dcb11ed9c1047
junbro <khm11904@gmail.com>
2022-06-15 21:14:20 +0900

    ♻️ 글로벌 폰트 및 스타일 수정

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,6 +9,12 @@ export default function Document() {
           async
           defer
         ></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <div id="portal" />
