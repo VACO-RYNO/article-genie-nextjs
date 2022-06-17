6489c1f25ae6815147567d11504d66f1817a89b8
Taewan Lim <taewan.seoul@gmail.com>
2022-06-17 16:21:58 +0900

    ✨ 메타 태그와 오픈그래프 이미지 추가

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,9 +21,27 @@ export default function MainPage() {
   return (
     <>
       <Head>
-        <title>ArticleGenie</title>
-        <meta name="description" content="article genie main page" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>
+          Article Genie - Web Article Reading Helper | Make Your Own Notes
+        </title>
+        <meta
+          name="description"
+          content="Highlight your thoughts reading web articles, and make your own notes out of it."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.articlegenie.co/" />
+        <meta
+          property="og:title"
+          content="Article Genie - Web Article Reading Helper | Make Your Own Notes"
+        />
+        <meta property="og:image" content="/images/genie-logo.png" />
+        <meta property="og:site_name" content="Article Genie" />
+        <meta
+          property="og:description"
+          content="Highlight your thoughts reading web articles, and make your own notes out of it."
+        />
+        <meta property="og:locale" content="en_US" />
+        <link rel="icon" href="/images/genie-favicon.png" />
       </Head>
       <Wrapper>
         <AddressBar />
